a55b6ebc08b7046478b91fcdd92c54dbc9804a78
tony283 <84143132+tony283@users.noreply.github.com>
2024-08-28 08:11:14 +0800

    1

--- data/contracts.py
+++ data/contracts.py
@@ -3,10 +3,16 @@ import rqdatac
 import pandas as pd
 rqdatac.init()
 from datetime import datetime
+from rescale import *
 import os
 import json
 import sys
 def timer(func):
+    """_summary_
+    装饰器计时
+    Args:
+        func (_type_): _description_
+    """
     def func_wrapper(*args,**kwargs):
         from time import time
         time_start = time()
@@ -17,6 +23,14 @@ def timer(func):
         return result
     return func_wrapper
 def str2list(s:str):
+    """_summary_
+        将str反序列化成list
+    Args:
+        s (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
     s=s.replace("[","")
     s=s.replace("]","")
     s=s.replace("'","")
@@ -49,11 +63,21 @@ def generate_list(dominant:str,contracts:list,switchback:bool)->str:
             if not flag:
                 contract_list.append(i)
     return contract_list
-# date = rqdatac.futures.get_dominant(future_type,start_date=start,end_date=end)
-# contracts = pd.DataFrame(index=date.index)
-# for i in date.index:
-#     contracts.loc[i,"contracts"] = rqdatac.futures.get_contracts(future_type, date=i)
-# contracts.to_excel(f"{future_type}_contracts.xlsx")
+
+def read_contracts(future_type,start,end):
+    """_summary_
+    生成每日所有未到期contract的dataframe，从米筐中获取数据
+
+    Args:
+        future_type (_type_): 期货代码
+        start (_type_): 起始日
+        end (_type_): 终止日
+    """
+    date = rqdatac.futures.get_dominant(future_type,start_date=start,end_date=end)
+    contracts = pd.DataFrame(index=date.index)
+    for i in date.index:
+        contracts.loc[i,"contracts"] = rqdatac.futures.get_contracts(future_type, date=i)
+    contracts.to_excel(f"data/{future_type}/{future_type}_contracts.xlsx")
 
 
 
@@ -118,32 +142,48 @@ class Contracts:
         return dominant_list
 
     def try_get_contract_daily(self,contract_name:str):
+        """_summary_
+
+        Args:
+            contract_name (str): 合约代码
+
+        Returns:
+            _type_: 合约历史数据
+        """
         month = int(contract_name[-2:])
         year = int(contract_name[-4:-2])
         if not os.path.exists(f"data/{contract_name[:-4]}/"):
-            os.makedirs(f"data/{contract_name[:-4]}/")
+            os.makedirs(f"data/{contract_name[:-4]}/")#没有创建过文件夹则创建
         if not os.path.exists(f"data/{contract_name[:-4]}/{contract_name}.xlsx"):
-            rqdatac.get_price(contract_name).to_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")
+            rqdatac.get_price(contract_name,adjust_type="post").to_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")#再从米筐读数据存入本地
         elif year<datetime.today().year or (year==datetime.today().year and month<datetime.today().month):
             pass
         else:
-            rqdatac.get_price(contract_name).to_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")
+            rqdatac.get_price(contract_name,adjust_type="post").to_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")#如果合约还未到期，需要实时更新
         try:
-            return self.data[contract_name]
+            return self.data[contract_name]#尝试从运行中的内存获取
         except:
-            self.data[contract_name] = pd.read_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")
+            self.data[contract_name] = pd.read_excel(f"data/{contract_name[:-4]}/{contract_name}.xlsx")#否则重新从本地读取
             return self.data[contract_name]
   
     def try_get_contract_instrument(self,contract_name:str):
+        """_summary_
+            获取期货合约具体信息
+        Args:
+            contract_name (str): 合约代码
+
+        Returns:
+            _type_: 合约具体信息(字典格式)
+        """
         info = None
 
         if not os.path.exists(f"data/{contract_name[:-4]}/"):
-            os.makedirs(f"data/{contract_name[:-4]}/")
+            os.makedirs(f"data/{contract_name[:-4]}/")#如果没获取过创建文件夹
         if not os.path.exists(f"data/{contract_name[:-4]}/{contract_name}.txt"):
             with open(f'data/{contract_name[:-4]}/{contract_name}.txt', 'w') as f:
-                f.write(json.dumps(rqdatac.instruments(contract_name).__dict__))
+                f.write(json.dumps(rqdatac.instruments(contract_name).__dict__))#如果获取过将其转为json
         try:
-            info = self.data[f"{contract_name}_instrument"]
+            info = self.data[f"{contract_name}_instrument"]#尝试从已有数据获取
         except:
             with open(f'data/{contract_name[:-4]}/{contract_name}.txt', 'r') as f:
                 info = json.loads(f.read())
@@ -172,12 +212,21 @@ class Contracts:
 
 
 def working_days(start,end):
+    """_summary_
+
+    Args:
+        start (_type_): 开始日期
+        end (_type_): 结束日期
+
+    Returns:
+        _type_: 返回两个日期间的工作日天数
+    """
     try:
-        start = datetime.fromtimestamp(start)
+        start = datetime.fromtimestamp(start)#格式化
     except:
         pass
     try:
-        end = datetime.fromtimestamp(end)
+        end = datetime.fromtimestamp(end)#格式化
     except:
         pass
         
@@ -194,7 +243,7 @@ class Condition:
     def __init__(self):
         pass
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        pass
+        return True
 
 
  #############################################################################################   
@@ -250,7 +299,7 @@ class DeListedCondition:
         return (delta_date >=self.days)
     
 class DeliveryMonthCondition:
-    """_summary_持仓量条件
+    """_summary_交割月条件
     """
     def __init__(self,days:int,working_day=True):
         self.days = days
@@ -261,11 +310,21 @@ class DeliveryMonthCondition:
         end_date = datetime(year,month,1)
         delta_date = working_days(date,end_date) if self.working_day else  end_date-date
         return (delta_date >=self.days)
-    
-        
-    
-test = Contracts() 
-test.process_contracts("CU","20190102","20240827",False,
-                  DeliveryMonthCondition(days=10,working_day=True),
-                  VolumeCondition(days=3,max_volume=1000)
-                  )
+
+
+def handle_contract(future_type,start,end,*conditions):
+    """
+    主函数，传入期货代码和时间
+    """    
+    read_contracts(future_type,start,end)    
+    test = Contracts() 
+    test.process_contracts(future_type,start,end,False,
+                    *conditions##可以在后面继续增加条件，也可以自定义条件
+                    )
+
+    m_data =pd.read_excel(f"data/{future_type}/{future_type}_dominant_result.xlsx")
+    # print(m_data.iloc[0])
+    transfer_dominant(m_data,future_type)
+
+if __name__=="__main__":
+    handle_contract("CU","20120102","20240827")
